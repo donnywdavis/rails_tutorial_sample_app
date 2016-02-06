23dd8f1c54b3ce6aa1cf91bb38322d0a49541bb0
Donny Davis <donnywdavis@icloud.com>
2016-02-06 15:00:16 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    # Get the user associated with the id parameter
+    @user = User.find(params[:id])
   end
+
+  def new
+    # Create a new user instance
+    @user = User.new
+  end
+
+  def create
+    # Create a new user instance with the data from the form
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
